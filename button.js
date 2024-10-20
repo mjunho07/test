4545ae30b3c648cc01cb6c50b98248e1351678f3
junho  myeong <kumgok.s230240@ggh.goe.go.kr>
2024-10-20 21:03:31 +0900

    thre

--- button.js
+++ button.js
@@ -1,1 +1,13 @@
-
+const button = document.querySelector('.button');
+function timeStart(){
+    const clickedClass = "clicked";
+    if(button.classList.contains(clickedClass)){
+        button.classList.remove(clickedClass);
+        button.innerText = "START"
+    }else {
+        button.classList.add(clickedClass);
+        
+        button.innerText = "STOP";
+    }
+}
+button.addEventListener("click",timeStart);
