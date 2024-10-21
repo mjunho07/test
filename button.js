c674ab9b9d16b30ffa1f6b94d8aa260196b75054
junho  myeong <kumgok.s230240@ggh.goe.go.kr>
2024-10-21 19:20:17 +0900

    test

--- button.js
+++ button.js
@@ -1,13 +1,17 @@
 const button = document.querySelector('.button');
-function timeStart(){
+const timer = document.querySelector('.time');
+function handleButton(){
     const clickedClass = "clicked";
     if(button.classList.contains(clickedClass)){
         button.classList.remove(clickedClass);
         button.innerText = "START"
     }else {
         button.classList.add(clickedClass);
-        
         button.innerText = "STOP";
+        setInterval(startTimer,1000);
     }
 }
-button.addEventListener("click",timeStart);
+function startTimer(){
+
+}
+button.addEventListener("click",handleButton);
