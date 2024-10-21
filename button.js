d8ee12d38ad529c3d5b7c50fca72c3a64d31b56c
mjunho07 <kumgok.s230240@ggh.goe.go.kr>
2024-10-21 11:39:56 +0900

    Add files via upload

--- button.js
+++ button.js
@@ -1,13 +1,22 @@
 const button = document.querySelector('.button');
+const reset = document.querySelector('.reset');
 function timeStart(){
     const clickedClass = "clicked";
     if(button.classList.contains(clickedClass)){
         button.classList.remove(clickedClass);
         button.innerText = "START"
+        console.log("타이머 정지");
     }else {
         button.classList.add(clickedClass);
-        
         button.innerText = "STOP";
+        console.log("타이머 시작,재개");
+    }
+}
+function onReset(){
+    const resetCheck = prompt("real?typing the 'reset'")
+    if(resetCheck == "reset"){
+        console.log("초기화");
     }
 }
 button.addEventListener("click",timeStart);
+reset.addEventListener("click",onReset);
