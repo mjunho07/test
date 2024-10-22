2c3cc79a31a4e4d4e11b8bbb83911f26388b6e2c
mjunho07 <kumgok.s230240@ggh.goe.go.kr>
2024-10-22 12:34:58 +0900

    up:reset,loop

--- button.js
+++ button.js
@@ -7,6 +7,8 @@ let interval;
 let minute = 60;
 let second = 0;
 
+const OENSECOND = 1000; 
+
 function handleTimer(){
     const clickedClass = "clicked";
     if(button.classList.contains(clickedClass)){
@@ -16,7 +18,7 @@ function handleTimer(){
     }else {
         button.classList.add(clickedClass);
         button.innerText = "STOP";
-        interval = setInterval(startTimer,1000);
+        interval = setInterval(startTimer,OENSECOND);
     }
 }
 function onReset(){
@@ -27,6 +29,8 @@ function onReset(){
         time.innerText = `${String(minute).padStart(2,"0")}:${String(second).padStart(2,"0")}`;
         time2.classList.add('hide');
         time.classList.remove('hide');
+        clearInterval(interval);
+        interval = setInterval(startTimer,OENSECOND);
     }
 }
 function startTimer(){
@@ -43,6 +47,27 @@ function startTimer(){
         clearInterval(interval);
         time2.classList.remove('hide');
         time.classList.add('hide');
+        minute = 5;
+        interval = setInterval(startTimer2,OENSECOND);
+    }
+}
+function startTimer2(){
+    if(second > 0){
+        second--;
+        time2.innerText = `${String(minute).padStart(2,"0")}:${String(second).padStart(2,"0")}`;
+    }
+    else if(minute > 0){
+        minute--;
+        second = 59;
+        time2.innerText = `${String(minute).padStart(2,"0")}:${String(second).padStart(2,"0")}`;
+    }else{
+        time2.innerText = `${String(minute).padStart(2,"0")}:${String(second).padStart(2,"0")}`;
+        clearInterval(interval);
+        time.classList.remove('hide');
+        time2.classList.add('hide');
+        minute = 60
+        time2.innerText = `${String(minute).padStart(2,"0")}:${String(second).padStart(2,"0")}`;
+        interval = setInterval(startTimer,OENSECOND);
     }
 }
 button.addEventListener("click",handleTimer);
